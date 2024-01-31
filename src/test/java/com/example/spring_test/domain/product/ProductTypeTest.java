186e300acb1dbef2fd941e16cbe60a652d859d1b
wisehero10 <godjw96@gmail.com>
2024-01-31 11:19:19 +0900

    #15 @ParameterizedTest

--- src/test/java/com/example/spring_test/domain/product/ProductTypeTest.java
+++ src/test/java/com/example/spring_test/domain/product/ProductTypeTest.java
@@ -4,6 +4,8 @@ import static org.assertj.core.api.Assertions.*;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 class ProductTypeTest {
 
@@ -32,5 +34,18 @@ class ProductTypeTest {
 		// then
 		assertThat(result).isTrue();
 	}
+
+	@DisplayName("상품 타입이 재고 관련 타입인지를 체크한다.")
+	@CsvSource({"HANDMADE, false", "BOTTLE, true", "BAKERY, true"})
+	@ParameterizedTest
+	void containsStockType4(ProductType productType, boolean expected) {
+		// when
+		boolean result = ProductType.containsStockType(productType);
+
+		//then
+		assertThat(result).isEqualTo(expected);
+	}
+
+
 
 }
