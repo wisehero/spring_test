dc9e9d46b7ff4caa1fd5839d2e01b4c613f7bb6f
wisehero10 <godjw96@gmail.com>
2024-02-01 20:16:51 +0900

    #16 @ParameterizedTest와 Stream<Arguments>

--- src/test/java/com/example/spring_test/domain/product/ProductTypeTest.java
+++ src/test/java/com/example/spring_test/domain/product/ProductTypeTest.java
@@ -2,12 +2,18 @@ package com.example.spring_test.domain.product;
 
 import static org.assertj.core.api.Assertions.*;
 
+import java.util.stream.Stream;
+
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.springframework.data.repository.query.Param;
 
 class ProductTypeTest {
+
 
 	@DisplayName("상품 타입이 재고 관련 타입인지를 체크한다.")
 	@Test
@@ -46,6 +52,22 @@ class ProductTypeTest {
 		assertThat(result).isEqualTo(expected);
 	}
 
+	private static Stream<Arguments> provideProductTypesForCheckingStockType() {
+		return Stream.of(
+				Arguments.of(ProductType.HANDMADE, false),
+				Arguments.of(ProductType.BOTTLE, true),
+				Arguments.of(ProductType.BAKERY, true)
+		);
+	}
 
+	@DisplayName("상품 타입이 재고 관련 타입인지를 체크한다.")
+	@MethodSource("provideProductTypesForCheckingStockType")
+	@ParameterizedTest
+	void containsStockType5(ProductType productType, boolean expected) {
+		// when
+		boolean result = ProductType.containsStockType(productType);
 
+		// then
+		assertThat(result).isEqualTo(expected);
+	}
 }
